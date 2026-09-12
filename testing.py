3830b08857dcdb15209bef52e7e2452bd664fc96
Rinkusagu <rinkusagu2004@gmail.com>
2026-09-12 15:55:51 +0530

    this is testing.py

---
+++ testing.py
@@ -0,0 +1,12 @@
+print ("hello world")
+print("Hello, World!")
+# Star Pattern Program
+
+for i in range(1, 6):
+    print("*" * i)
+print("5+5")
+for i in range(3):
+    print("Loop1:", i)
+
+for j in range(2):
+    print("Loop2:", j)
